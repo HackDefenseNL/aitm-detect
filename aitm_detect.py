c6bb008fa266edd06465aae31819afc10b5b1f20
Niels van Gijzen <n.van.gijzen@gmail.com>
2024-05-15 11:57:13 +0200

    Inverted if statement checks in order to make the code less indented

--- aitm_detect.py
+++ aitm_detect.py
@@ -13,12 +13,15 @@ BANNER = f"""
 ===============================================
 """
 
+
 def extract_custom_css_url(response_text):
     match = re.search(r'customCssUrl":"(https://[^"]+)', response_text)
     return match.group(1) if match else None
 
+
 def extract_image_urls(css_content):
     return re.findall(r'url\((.*?)\)', css_content)
+
 
 def main(url):
     print(BANNER)
@@ -26,32 +29,35 @@ def main(url):
     full_url = f"https://login.microsoftonline.com/?whr={url}"
     response = requests.get(full_url)
 
-    if response.status_code == 200:
-        print(f"[{GREEN}+{RESET}] {url} domain is valid!")
-        custom_css_url = extract_custom_css_url(response.text)
-
-        if custom_css_url:
-            css_response = requests.get(custom_css_url)
-            
-            print(f"[{GREEN}+{RESET}] Custom CSS company branding found! Fetching Custom CSS content from:\n{custom_css_url}")
-
-            if css_response.status_code == 200:
-                image_urls = extract_image_urls(css_response.text)
-
-                if image_urls:
-                    print(f"[{GREEN}+{RESET}] External (image) URLs found in CSS content, possibly used as anti-AiTM measure(s):")
-                    for img_url in image_urls:
-                        print(img_url.strip('\'"'))
-                else:
-                    print(f"[{RED}-{RESET}] No (image) URLs found in the CSS content. Manually review the CSS content!")
-                    print(f"[{ORANGE}!{RESET}] Printing company branding custom CSS content:\n{css_response.text}")
-
-            else:
-                print(f"[{RED}-{RESET}] Failed to fetch custom CSS content. Status code: {css_response.status_code}")
-        else:
-            print(f"[{RED}-{RESET}] No 'customCssUrl' found in the response. There is most likely no custom CSS company branding!")
-    else:
+    if response.status_code != 200:
         print(f"[{RED}-{RESET}] Failed to fetch content. Status code: {response.status_code}")
+        return
+
+
+    print(f"[{GREEN}+{RESET}] {url} domain is valid!")
+    custom_css_url = extract_custom_css_url(response.text)
+
+    if custom_css_url is None:
+        print(f"[{RED}-{RESET}] No 'customCssUrl' found in the response. There is most likely no custom CSS company branding!")
+        return
+
+    css_response = requests.get(custom_css_url)
+    print(f"[{GREEN}+{RESET}] Custom CSS company branding found! Fetching Custom CSS content from:\n{custom_css_url}")
+
+    if css_response.status_code != 200:
+        print(f"[{RED}-{RESET}] Failed to fetch custom CSS content. Status code: {css_response.status_code}")
+        return
+
+    image_urls = extract_image_urls(css_response.text)
+
+    if image_urls:
+        print(f"[{GREEN}+{RESET}] External (image) URLs found in CSS content, possibly used as anti-AiTM measure(s):")
+        for img_url in image_urls:
+            print(img_url.strip('\'"'))
+    else:
+        print(f"[{RED}-{RESET}] No (image) URLs found in the CSS content. Manually review the CSS content!")
+        print(f"[{ORANGE}!{RESET}] Printing company branding custom CSS content:\n{css_response.text}")
+
 
 def domains_from_file(hosts):
     with open(hosts, 'r') as file:
@@ -59,6 +65,7 @@ def domains_from_file(hosts):
             domain = line.strip()
             if domain:
                 main(domain)
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Check custom CSS company branding for AiTM measures")
